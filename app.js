d809e9dff677383d0a9142fd2f709d58f55719e2
kongaman <christian.kranz@gmail.com>
2018-05-31 00:31:11 +0200

    installed body-parser & created server and client side form validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -19,6 +20,10 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Index Route
 app.get('/', (req, res) => {
     const title = 'Welcome!'
@@ -34,6 +39,26 @@ app.get('/about', (req, res) => {
 //Add Idea form
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
+});
+
+//Process form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({text: 'Please add a title'})
+    }
+    if (!req.body.details) {
+        errors.push({text: 'Please add some details'})
+    }
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });    
+    } else {
+        res.send('passed');
+    }
 });
 
 const port= 5000;
