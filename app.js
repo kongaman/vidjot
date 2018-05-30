14f013132ee2fff9f9e2d27235e610f57bd44e02
kongaman <christian.kranz@gmail.com>
2018-05-30 22:55:37 +0200

    Use templates (express-handlebars)

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const port= 5000;
 const app = express();
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    //What you want on every page (views/layout/main.handlebars)
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 //Index Route
 app.get('/', (req, res) => {
-    res.send('INDEX');
+    const title = 'Welcome!'
+    res.render('INDEX', {
+        title: title
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('ABOUT');
+    res.render('ABOUT');
 });
 
 
