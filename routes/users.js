1272d730bf21dc18360824d0ba7e15840ee9eb0a
kongaman <christian.kranz@gmail.com>
2018-06-16 18:11:34 +0200

    logout functionality created

--- routes/users.js
+++ routes/users.js
@@ -77,5 +77,11 @@ router.post('/register', (req, res) => {
     }
 });
 
+//Lofout User
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'Successfully logged out');
+    res.redirect('/users/login');
+});
 
 module.exports = router
